8be0ac3d53b5c9549362840ab7a534d6f1ed5056
nickyim <nicholasjyim@gmail.com>
2023-10-07 14:21:07 -0400

    setup login

--- mentalhealth/src/App.js
+++ mentalhealth/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React, { useState } from 'react';
 import Login from './pages/Login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div className="App">
-      <Login />
+      {isLoggedIn ? (
+        <div>
+          Welcome to the User Homepage!
+          {/* Placeholder for your UserSubmissions component */}
+        </div>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
     </div>
   );
 }
--- mentalhealth/src/pages/Login.jsx
+++ mentalhealth/src/pages/Login.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
+import './Login.css';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isProfessional, setIsProfessional] = useState(false);
+  const [professionalCode, setProfessionalCode] = useState('');
 
   const handleLogin = () => {
-    // Handle login logic here (like sending a request to your server)
-    console.log(`Logging in with username: ${username} and password: ${password}`);
+    // You can put your login logic here.
+    // For now, I'll just log to the console.
+    console.log('Login button clicked!');
   };
 
   return (
-    <div className="login-container">
+    <div className="landing-container">
       <h1>Welcome to Mental Health App</h1>
       
       <div className="login-form">
@@ -26,6 +30,26 @@ function Login() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        
+        <div className="professional-check">
+          <input
+            type="checkbox"
+            id="professional"
+            checked={isProfessional}
+            onChange={() => setIsProfessional(!isProfessional)}
+          />
+          <label htmlFor="professional">I am a professional</label>
+        </div>
+
+        {isProfessional && (
+          <input
+            type="text"
+            placeholder="Enter professional code"
+            value={professionalCode}
+            onChange={(e) => setProfessionalCode(e.target.value)}
+          />
+        )}
+
         <button onClick={handleLogin}>
           Login
         </button>
