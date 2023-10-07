641852e09a77dcb53bc4972ef5f45bc05da2814e
Aseef Durrani <aseef@nbp-29-5.nbp.ruw.rutgers.edu>
2023-10-07 17:55:50 -0400

    zerosugar

--- mentalhealth/src/pages/userHome.jsx
+++ mentalhealth/src/pages/userHome.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
-import './UserHome.css';
-//import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-function UserHome({ userName }) { // userName prop to display user's name
+import React, { useState } from "react";
+import "./UserHome.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container } from "react-bootstrap";
+
+function UserHome({ userName }) {
+  const [submission, setSubmission] = useState("");
+
+  const handleSubmissionChange = (event) => {
+    setSubmission(event.target.value);
+  };
+
+  const handleSubmission = () => {
+    //this is where data will be sent to firebase
+    console.log(submission);
+  };
+
   return (
     <div className="user-home-container">
+      <Container>
+        <h1 className="welcome-header" style={{ textAlign: "center" }}>
+          Welcome {userName}! Nice to see you!
+        </h1>
+        <div className="text-center">
+          <button className="submissions-button">
+            <a href="#submissions">To Submissions</a>
+          </button>
+        </div>
 
-      {/* Welcome header */}
-      <h1 style={{textAlign : 'center'}} >Welcome {userName}! Nice to see you!</h1>
+        <div className="mission-statement">
+          <h2 className="mission-header" style={{ textAlign: "center" }}>
+            "[write mission statement here]"
+          </h2>
+          <p className="funies-text"> "funies"</p>
+        </div>
 
-      {/* "To Submissions" button */}
-      <button>
-        <a href="#submissions">To Submissions</a>
-      </button>
-
-      {/* Mission statement */}
-      <div className="mission-statement">
-        <h2 style ={{textAlign : 'center'}}> "[write mission statement here]"</h2>
-        <p style={{textAlign: 'center'}}> "funies"</p>
-      </div>
-
-      {/* "Your Submissions:" section */}
-      <div id="submissions" className="submissions-section">
-        <h3>Your Submissions:</h3>
-        <textarea defaultValue="[Your submissions content here]"></textarea>
-      </div>
-
+        <div id="submissions" className="submissions-section">
+          <h3>Your Submissions:</h3>
+          <textarea
+            value={submission}
+            onChange={handleSubmissionChange}
+            className="submission-textarea"
+          ></textarea>
+          <button onClick={handleSubmission} className="submit-button">
+            Submit
+          </button>
+        </div>
+      </Container>
     </div>
   );
 }
