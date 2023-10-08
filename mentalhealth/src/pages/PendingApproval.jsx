7251b55586672abd1c509145018f8d1ad9c0e5e3
Maanav Choudhary <emperormaanav@nbp-26-148.nbp.ruw.rutgers.edu>
2023-10-08 09:39:39 -0400

    prof_authenticate

--- mentalhealth/src/pages/PendingApproval.jsx
+++ mentalhealth/src/pages/PendingApproval.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { db, auth } from '../firebase-config'; // Adjust the path
+import { ref, get } from 'firebase/database';
 import './PendingApproval.css';
 
 function PendingApproval() {
+    const navigate = useNavigate();
+    const intervalRef = useRef(null); // To hold reference of the interval
+
+    useEffect(() => {
+        const checkAccess = async () => {
+            // Ensure there's a logged-in user
+            if (auth.currentUser) {
+                const uid = auth.currentUser.uid;
+
+                // Create a reference to the user's "access" field in the database
+                const accessRef = ref(db, 'users/' + uid + '/access');
+
+                try {
+                    const snapshot = await get(accessRef);
+
+                    if (snapshot.exists()) {
+                        const access = snapshot.val();
+                        
+                        if (access === 1) {
+                            clearInterval(intervalRef.current); // Clear interval once the desired condition is met
+                            navigate('/profhome');
+                        } else if (access === 0) {
+                            clearInterval(intervalRef.current); // Clear interval once the desired condition is met
+                        }
+                    } else {
+                        console.error("Access field not found for user:", uid);
+                    }
+                } catch (error) {
+                    console.error("Error checking access field:", error);
+                }
+            }
+        };
+
+        intervalRef.current = setInterval(checkAccess, 10000); // Check every 10 seconds
+
+        return () => {
+            clearInterval(intervalRef.current); // Clear the interval if the component unmounts
+        };
+    }, [navigate]); // Execute only once when the component mounts
+
     return (
         <div className="pending-approval-container text-center">
             <div className="icon-container">
--- mentalhealth/src/pages/RegistrationPage.jsx
+++ mentalhealth/src/pages/RegistrationPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import UserRegistration from './UserRegistration';
 import ProfRegistration from './ProfRegistration';
+import {db,auth} from "../firebase-config";
+import {ref,get} from 'firebase/database';
 import { Tabs, Tab } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './RegistrationPage.css';
@@ -8,10 +10,44 @@ import './RegistrationPage.css';
 function RegistrationPage() {
   const [key, setKey] = useState('user'); // Default tab
   const navigate = useNavigate();
+  const handleRegistrationSuccess = async () => {
+    try {
+      // Get the current user's UID
+      const uid = auth.currentUser.uid;
 
-  const handleRegistrationSuccess = (path = '/') => {
-    navigate(path, { state: { fromRegistration: true } });
-  };
+      // Create a reference to the user's "proffesional" field in the database
+      const professionalRef = ref(db, 'users/' + uid + '/proffesional');
+      const accessRef = ref(db, 'users/' + uid + '/access');
+      const ss = await get(accessRef);
+      const access = ss.val();
+      // Fetch the value of "proffesional" from the database
+      const snapshot = await get(professionalRef);
+
+      if (snapshot.exists()) {
+          const proffesional = snapshot.val();
+          
+          if (proffesional === 1) {
+            if(access == 1){
+              navigate('/profhome');
+            }else{
+              navigate('/pending-approval');
+            }
+          } else if (proffesional === 0) {
+              navigate('/userhome');
+          } else {
+              // Default navigation if "proffesional" is neither 0 nor 1
+              navigate('/', { state: { fromRegistration: true } });
+          }
+      } else {
+          // If "proffesional" field doesn't exist in the database, navigate to default
+          console.error("Proffesional field not found for user:", uid);
+          navigate('/', { state: { fromRegistration: true } });
+      }
+  } catch (error) {
+      console.error("Error checking proffesional field:", error);
+      navigate('/', { state: { fromRegistration: true } });
+  }
+};
 
   return (
     <div className="registration-page-container">
